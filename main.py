532973da69ceb13963a32ff51d912ad08c585268
Rublood <ciesielski.remi@gmail.com>
2019-05-30 22:35:44 +0200

    start cli

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from datetime import datetime
 
 from scrap import *
 from rss.pika import Pika
+import click
 
 
 def group_by(dict_list, key):
@@ -14,15 +15,31 @@ def group_by(dict_list, key):
     return tmp
 
 
-def print_data(dict_list):
+def print_data(dict_list, reverse):
     grouped_by = group_by(dict_list, 'date')
-    for date in sorted(grouped_by.items(), key=lambda x: datetime.strptime(x[0], '%d/%m/%Y'), reverse=True):
+    for date in sorted(grouped_by.items(), key=lambda x: datetime.strptime(x[0], '%d/%m/%Y'), reverse=reverse):
         for item in grouped_by[date[0]]:
             print("%s\n" % item)
 
 
-editor = [Glenat, Kaze, Kana, Pika]
-res = []
-for x in editor:
-    res.extend(x().res)
-print_data(res)
+default_editor = ["Glenat", "Kaze", "Kana", "Pika"]
+
+
+@click.command()
+@click.option('--editor', '-e', default=default_editor,
+              help='Choose your editor can be multiple resources available(%s)' % default_editor,
+              type=click.Choice(default_editor), multiple=True)
+@click.option('--reverse', '-r', is_flag=True, default=True, help='reverse the date sort')
+def schega(editor, reverse):
+    res = []
+    if editor:
+        default_editor = list(editor)
+    for x in range(len(default_editor)):
+        default_editor[x] = eval(default_editor[x])
+    for x in default_editor:
+        res.extend(x().res)
+    print_data(res, reverse)
+
+
+if __name__ == '__main__':
+    schega()
